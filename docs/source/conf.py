4c8558c896a52b3269a541fd2cbb7d424a709cbd
Harry Brundage <harry.brundage@gmail.com>
2015-04-17 17:12:26 -0400

    Add python3 compatability

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,8 +44,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'pywebhdfs'
-copyright = u'2013, Steven D. Gonzales'
+project = 'pywebhdfs'
+copyright = '2013, Steven D. Gonzales'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -190,8 +190,8 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual])
 latex_documents = [
-    ('index', 'pywebhdfs.tex', u'pywebhdfs Documentation',
-     u'Steven D. Gonzales', 'manual'),
+    ('index', 'pywebhdfs.tex', 'pywebhdfs Documentation',
+     'Steven D. Gonzales', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -220,8 +220,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'pywebhdfs', u'pywebhdfs Documentation',
-     [u'Steven D. Gonzales'], 1)
+    ('index', 'pywebhdfs', 'pywebhdfs Documentation',
+     ['Steven D. Gonzales'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -234,8 +234,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    ('index', 'pywebhdfs', u'pywebhdfs Documentation',
-     u'Steven D. Gonzales', 'pywebhdfs', 'One line description of project.',
+    ('index', 'pywebhdfs', 'pywebhdfs Documentation',
+     'Steven D. Gonzales', 'pywebhdfs', 'One line description of project.',
      'Miscellaneous'),
 ]
 
--- example/example.py
+++ example/example.py
@@ -17,50 +17,50 @@ hdfs = PyWebHdfsClient(host='localhost', port='50070',
 
 
 # create a new directory for the example
-print('making new HDFS directory at: {0}\n'.format(example_dir))
+print(('making new HDFS directory at: {0}\n'.format(example_dir)))
 hdfs.make_dir(example_dir)
 
 # get a dictionary of the directory's status
 dir_status = hdfs.get_file_dir_status(example_dir)
-print dir_status
+print(dir_status)
 
 # create a new file on hdfs
-print('making new file at: {0}\n'.format(example_file))
+print(('making new file at: {0}\n'.format(example_file)))
 hdfs.create_file(example_file, example_data)
 
 file_status = hdfs.get_file_dir_status(example_file)
-print file_status
+print(file_status)
 
 # append to the file created in previous step
-print('appending to file at: {0}\n'.format(example_file))
+print(('appending to file at: {0}\n'.format(example_file)))
 hdfs.append_file(example_file, example_data)
 
 file_status = hdfs.get_file_dir_status(example_file)
-print file_status
+print(file_status)
 
 # read in the data for the file
-print('reading data from file at: {0}\n'.format(example_file))
+print(('reading data from file at: {0}\n'.format(example_file)))
 file_data = hdfs.read_file(example_file)
-print file_data
+print(file_data)
 
 # rename the example_dir
-print('renaming directory from {0} to {1}\n').format(example_dir, rename_dir)
+print(('renaming directory from {0} to {1}\n').format(example_dir, rename_dir))
 hdfs.rename_file_dir(example_dir, '/{0}'.format(rename_dir))
 
 # list the contents of the new directory
 listdir_stats = hdfs.list_dir(rename_dir)
-print listdir_stats
+print(listdir_stats)
 
 example_file = '{dir}/example.txt'.format(dir=rename_dir)
 
 # delete the example file
-print('deleting example file at: {0}'.format(example_file))
+print(('deleting example file at: {0}'.format(example_file)))
 hdfs.delete_file_dir(example_file)
 
 # list the contents of the directory
 listdir_stats = hdfs.list_dir(rename_dir)
-print listdir_stats
+print(listdir_stats)
 
 # delete the example directory
-print('deleting the example directory at: {0}'.format(rename_dir))
+print(('deleting the example directory at: {0}'.format(rename_dir)))
 hdfs.delete_file_dir(rename_dir, recursive='true')
